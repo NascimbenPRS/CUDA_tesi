7318ba958846f2e8af1d0a021aa45621c76cc6a7
NascimbenPRS <54145621+NascimbenPRS@users.noreply.github.com>
2019-08-27 23:30:21 +0200

    Add files via upload

---
+++ commonFunctions.h
@@ -0,0 +1,25 @@
+// allocate memory using "cudaMallocManaged" if compiled with NVCC, "malloc" else
+void genericMalloc(void **ptr, int size, int onCPU) {
+	// compiler= nvcc
+#ifdef __NVCC__
+	cudaMallocManaged(ptr, size);
+#endif
+
+	// compiler!= nvcc
+#ifndef __NVCC__
+	*ptr = malloc(size);
+#endif
+}
+
+// free memory, using "cudaFree" if compiled with NVCC, "free" else
+void genericFree(void *ptr, int onCPU) {
+    // compiler= nvcc
+#ifdef __NVCC__
+	cudaFree(ptr);
+#endif
+
+	// compiler!= nvcc
+#ifndef __NVCC__
+	free(ptr);
+#endif
+}
